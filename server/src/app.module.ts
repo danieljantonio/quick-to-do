06add3499d55cc74c405e2f6f9f90f9cc6437221
Daniel Antonio <djedidiaha@gmail.com>
2023-06-15 14:14:22 +0700

    base todos CRUD template

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
