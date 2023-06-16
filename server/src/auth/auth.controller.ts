1596eddfee04fdc7da25b29d94e2ba5328f7ecab
Daniel J. Antonio <djedidiaha@gmail.com>
2023-06-17 04:45:25 +0700

    add endpoint to authorize admins

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
 import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { Request } from 'express';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -11,5 +12,10 @@ export class AuthController {
 	@Post('login')
 	login(@Body() { username }: LoginDto) {
 		return this.authService.login(username);
+	}
+
+	@Get('/')
+	authorizeUser(@Req() req: Request) {
+		return this.authService.authorizeUser(req);
 	}
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,8 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+} from '@nestjs/common';
 
 import { AuthEntity } from './entity/auth.entity';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ReqUser } from 'src/users/users.service';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthService {
@@ -24,5 +30,18 @@ export class AuthService {
 				{ expiresIn: '6h' },
 			),
 		};
+	}
+
+	async authorizeUser(req: Request) {
+		const _user = req.user as ReqUser;
+		if (!_user) throw new BadRequestException(`No user found`);
+
+		const user = await this.prisma.user.findFirst({
+			where: { id: _user.id, role: 'ADMIN' },
+		});
+
+		if (!user) return false;
+
+		return true;
 	}
 }
