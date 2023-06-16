33e013428be3c9c94ed9dc81d29e884805db94b7
Daniel J. Antonio <djedidiaha@gmail.com>
2023-06-16 14:52:48 +0700

    add auth guard

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-  .setTitle('Quick ToDo')
-  .setDescription('Quick Todo API Description')
-  .setVersion('0.1')
-  .build()
-  
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+	const config = new DocumentBuilder()
+		.setTitle('Quick ToDo')
+		.setDescription('Quick Todo API Description')
+		.setVersion('0.1')
+		.addBearerAuth()
+		.build();
 
-  await app.listen(5000);
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
+	await app.listen(5000);
 }
 bootstrap();
--- server/src/todos/todos.controller.ts
+++ server/src/todos/todos.controller.ts
@@ -6,11 +6,13 @@ import {
 	Patch,
 	Param,
 	Delete,
+	UseGuards,
 } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('todos')
 @ApiTags('todos')
@@ -18,26 +20,36 @@ export class TodosController {
 	constructor(private readonly todosService: TodosService) {}
 
 	@Post()
+	@UseGuards(JwtAuthGuard)
+	@ApiBearerAuth()
 	create(@Body() createTodoDto: CreateTodoDto) {
 		return this.todosService.create(createTodoDto);
 	}
 
 	@Get()
+	@UseGuards(JwtAuthGuard)
+	@ApiBearerAuth()
 	findAll() {
 		return this.todosService.findAll();
 	}
 
 	@Get(':id')
+	@UseGuards(JwtAuthGuard)
+	@ApiBearerAuth()
 	findOne(@Param('id') id: string) {
 		return this.todosService.findOne(id);
 	}
 
 	@Patch(':id')
+	@UseGuards(JwtAuthGuard)
+	@ApiBearerAuth()
 	update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
 		return this.todosService.update(id, updateTodoDto);
 	}
 
 	@Delete(':id')
+	@UseGuards(JwtAuthGuard)
+	@ApiBearerAuth()
 	remove(@Param('id') id: string) {
 		return this.todosService.remove(id);
 	}
