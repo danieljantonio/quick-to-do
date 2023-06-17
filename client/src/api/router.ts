0adf3675e43cfd12d006f93079ce3e5b969d2c52
Daniel J. Antonio <djedidiaha@gmail.com>
2023-06-18 01:21:10 +0700

    set init user to be user1

--- client/src/api/router.ts
+++ client/src/api/router.ts
@@ -4,16 +4,13 @@ import axios from 'axios';
 import date from 'date-and-time';
 
 // Auth configs
-const user1Token =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbGl5OGJwc2cwMDA2dHlyZnBzenN3ZmN1IiwiaWF0IjoxNjg2OTM3MDUzLCJleHAiOjE2ODY5NTg2NTN9.oIeh22BKfPrCeIUkgATV_Hi5ztegSqrj-G51y1svBFY';
-const user2Token =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbGl5OGJwc28wMDA4dHlyZnZwZ3dqZXg2IiwiaWF0IjoxNjg2OTUyMjc4LCJleHAiOjE2ODY5NzM4Nzh9.3nfy7x1KvI2HIxVmn070W94GdPX9dUGtsW55XLLw0To';
-const adminToken =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbGl6M3hyOGQwMDBhdHl5bmowN3NxNXUyIiwiaWF0IjoxNjg3MDIwNDUyLCJleHAiOjE2ODcwNDIwNTJ9.FMSA47XyeyqldKrXIB04k1iyHGBNLMxbPQ5Pk0VARbg';
+const user1Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbGl5OGJwc2cwMDA2dHlyZnBzenN3ZmN1IiwiaWF0IjoxNjg3MDI1NjA4LCJleHAiOjE2ODcwNDcyMDh9.VqSz8iN0BTE6wxdV6-KGV--MMxLeLOmDjK6NdSwV4cU';
+const user2Token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbGl5OGJwc28wMDA4dHlyZnZwZ3dqZXg2IiwiaWF0IjoxNjg3MDI1NjI5LCJleHAiOjE2ODcwNDcyMjl9.RwW3FR8UvM4GtMHV4Mp20NVgsCkEV9rWcbnsS_OhHwU';
+const adminToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbGl6M3hyOGQwMDBhdHl5bmowN3NxNXUyIiwiaWF0IjoxNjg3MDIwNDUyLCJleHAiOjE2ODcwNDIwNTJ9.FMSA47XyeyqldKrXIB04k1iyHGBNLMxbPQ5Pk0VARbg';
 
 const config = {
 	headers: {
-		Authorization: 'Bearer ' + adminToken,
+		Authorization: 'Bearer ' + user1Token,
 	},
 };
 
@@ -44,59 +41,32 @@ export const getCategories = () => {
 
 // To do routers
 export const getTodosToday = () => {
-	return axios.get<Todo[]>(
-		`http://localhost:5000/todos?dateStart=${new Date().toISOString()}&dateEnd=${new Date().toISOString()}`,
-		config,
-	);
+	return axios.get<Todo[]>(`http://localhost:5000/todos?dateStart=${new Date(setDateStart(new Date())).toISOString()}&dateEnd=${new Date(setDateEnd(new Date())).toISOString()}`, config);
 };
 
 export const getTodos = (query?: TodoQuery) => {
-	return axios.get<Todo[]>(
-		`http://localhost:5000/todos?${
-			query ? new URLSearchParams(query).toString() : ''
-		}`,
-		config,
-	);
+	return axios.get<Todo[]>(`http://localhost:5000/todos?${query ? new URLSearchParams(query).toString() : ''}`, config);
 };
 
 export const updateTodoStatus = (data: { id: string; isDone: boolean }) => {
-	return axios.patch<Todo>(
-		`http://localhost:5000/todos/${data.id}`,
-		{ isDone: data.isDone },
-		config,
-	);
+	return axios.patch<Todo>(`http://localhost:5000/todos/${data.id}`, { isDone: data.isDone }, config);
 };
 
-export const postTodo = (data: {
-	description: string;
-	dueAt: string;
-	categoryId?: string;
-}) => {
+export const postTodo = (data: { description: string; dueAt: string; categoryId?: string }) => {
 	return axios.post<Todo>('http://localhost:5000/todos', data, config);
 };
 
 export const deleteTodo = (id: string) => {
-	return axios.delete<{ removed: boolean }>(
-		`http://localhost:5000/todos/${id}`,
-		config,
-	);
+	return axios.delete<{ removed: boolean }>(`http://localhost:5000/todos/${id}`, config);
 };
 
 // Admin Routes
 export const authorizeAdmin = () => {
-	return axios.get<{ authorized: boolean }>(
-		'http://localhost:5000/auth',
-		config,
-	);
+	return axios.get<{ authorized: boolean }>('http://localhost:5000/auth', config);
 };
 
 export const getAdminTodos = (query?: TodoQuery) => {
-	return axios.get<Todo[]>(
-		`http://localhost:5000/todos/admin?${
-			query ? new URLSearchParams(query).toString() : ''
-		}`,
-		config,
-	);
+	return axios.get<Todo[]>(`http://localhost:5000/todos/admin?${query ? new URLSearchParams(query).toString() : ''}`, config);
 };
 
 export const getPastWeekStats = () => {
@@ -105,10 +75,7 @@ export const getPastWeekStats = () => {
 	const dateStart = date.addDays(dateEnd, -7);
 	dateStart.setHours(0, 0, 0, 0);
 
-	return axios.get<TodoStatsReturn>(
-		`http://localhost:5000/todos/stats?createdStart=${dateStart.toISOString()}&createdEnd=${dateEnd.toISOString()}`,
-		config,
-	);
+	return axios.get<TodoStatsReturn>(`http://localhost:5000/todos/stats?createdStart=${dateStart.toISOString()}&createdEnd=${dateEnd.toISOString()}`, config);
 };
 
 export const getNextWeekStats = () => {
@@ -117,8 +84,5 @@ export const getNextWeekStats = () => {
 	const dateEnd = date.addDays(dateStart, 7);
 	dateEnd.setHours(23, 59, 59, 999);
 
-	return axios.get<TodoStatsReturn>(
-		`http://localhost:5000/todos/stats?dateStart=${dateStart.toISOString()}&dateEnd=${dateEnd.toISOString()}`,
-		config,
-	);
+	return axios.get<TodoStatsReturn>(`http://localhost:5000/todos/stats?dateStart=${dateStart.toISOString()}&dateEnd=${dateEnd.toISOString()}`, config);
 };
